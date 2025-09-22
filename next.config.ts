14d93677e9cce5d3bb07041f3bbb74e1866435c9
Shrvan Benke <154585738+pix-panther24@users.noreply.github.com>
2025-09-22 20:05:40 +0530

    feat: update configuration

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   webpack(config) {
-    // Add SVGR loader for SVGs
+    config.module.rules = config.module.rules.map((rule: any) => {
+      if (rule.test?.test?.(".svg")) {
+        return { ...rule, exclude: /\.svg$/i };
+      }
+      return rule;
+    });
+
+    // Add SVGR for SVGs
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
       use: ["@svgr/webpack"],
     });
+
     return config;
   },
 };
--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,25 +1,32 @@
 import Image from "next/image";
+import { Menu } from "lucide-react";
 
 export const Header = () => {
   return (
-    <header className="py-4">
+    <header className="py-4 border-b border-white/15">
       <div className="container">
         <div className="flex justify-between">
           <div>
-            <Image
-              src="/logo.svg"
-              alt="Logo"
-              width={40}
-              height={40}
-              priority
-            />
+            <div className="border rounded-lg h-10 w-10 inline-flex justify-center items-center border-white/15">
+              <Image
+                src="/logo.svg"
+                alt="Logo"
+                width={40}
+                height={40}
+              />
+            </div>
           </div>
-          <div>
-            <button>
-              Get Started
+          <div className="flex gap-4 items-center">
+            <button className="relative py-2 px-3 rounded-lg font-medium text-sm bg-gradient-to-b from-[#190d2e] to-[#4a208a] shadow-[0px_0px_12px_#8c45ff]">
+              <div className="absolute inset-0">
+                <div className="rounded-lg border border-white/20 absolute inset-0 [mask-image:linear-gradient(to_bottom,black,transparent)]"></div>
+                <div className="rounded-lg border absolute inset-0 border-white/40 [mask-image:linear-gradient(to_bottom,black,transparent)]"></div>
+                <div className="absolute inset-0 shadow-[0_0_10px_rgb(140,69,255,.7)_inset] rounded-lg"></div>
+              </div>
+              <span>Get Started</span>
             </button>
             <span>
-              menu
+              <Menu size={32} color="white" />
             </span>
           </div>
         </div>
